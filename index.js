3cb356383d42030efee97bd4b48714e154ef5fc2
John P Tomanelli II <jptomanelli@gmail.com>
2018-04-11 19:14:19 -0400

    auth flow

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const keys = require('./config/keys');
 const app = express();
+
+mongoose.connect(keys.MONGO_URI);
+require('./models/User');
+require('./services/passport');
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.COOKIE_KEY]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-    res.send("Hi there");
-});
-
 app.listen(PORT);
